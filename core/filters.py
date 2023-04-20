afdc8a89fb27454246c9cfc47c5d9ac235ace503
João Teixeira <jloiola6@outlook.com>
2023-04-20 12:14:02 -0300

    Adicinando filtro por intervalo

--- core/filters.py
+++ core/filters.py
@@ -4,9 +4,11 @@ def filter_table(request):
     consumer = Consumer.objects.all().order_by('-id')
 
     type = request.GET.get('type')
+    range = request.GET.get('range')
     if type:
         consumer = consumer.filter(discount_rules__consumer_type=type)
         
-    print(request.GET.get('range'))
+    if range:
+        consumer = consumer.filter(discount_rules__consumption_range=range)
 
     return consumer
